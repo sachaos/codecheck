6bfca9f8981e81fffc6be61b794aed2009758bc3
shunjikonishi <swandouble@gmail.com>
2015-07-26 14:38:05 +0900

    API. Implement signin and getChallenge(temporally)

--- src/api.js
+++ src/api.js
@@ -1,7 +1,32 @@
 "use strict";
 
+var Promise = require("bluebird");
+var request = Promise.promisify(require("request").defaults({jar: true}));
+
 function API(host) {
   this.baseUrl = (host.indexOf("localhost") === 0 ? "http://" : "https://") + host;
 }
+
+API.prototype.signin = function(username, password) {
+  var options = {
+    url: this.baseUrl + "/api/auth/signin",
+    method: "POST",
+    form: {
+      nameOrEmail: username,
+      password: password
+    },
+    json: true
+  };
+  return request(options);
+};
+
+API.prototype.getChallenge = function(id) {
+  var options = {
+    url: this.baseUrl + "/api/challenges/" + id,
+    method: "GET",
+    json: true
+  };
+  return request(options);
+};
 
 module.exports = API;
