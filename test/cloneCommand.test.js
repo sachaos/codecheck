529ea5231ee7e22786d1f4447fc3fd87f7cd7b1b
shunjikonishi <swandouble@gmail.com>
2015-07-28 13:05:46 +0900

    Add test

---
+++ test/cloneCommand.test.js
@@ -0,0 +1,104 @@
+"use strict";
+
+var config = require("./config.json");
+var app    = require(config.code_main_dir + "/specs/utils/AppUtils");
+var db     = require(app.fromSpecRoot("/utils/DBUtils"));
+var assert = require("chai").assert;
+var rimraf = require("rimraf");
+var fs     = require("fs");
+
+var API          = require("../src/api");
+var CloneCommand = require("../src/commands/clone");
+
+describe("CloneCommand", function() {
+  this.timeout(5000);
+
+  var api = new API(config.host);
+  var resultId;
+
+  before(function(done) {
+    initData(function(data) {
+      resultId = data.challenge_results[0].id;
+      done();
+    });
+  });
+
+  beforeEach(function(done) {
+    rimraf("shunjikonishi-" + resultId, done);
+  });
+
+  after(function(done) {
+    rimraf("shunjikonishi-" + resultId, done);
+  });
+
+  it("should be exception with no args", function(done) {
+    var command = new CloneCommand(api);
+    try {
+      command.run([], {});
+      assert.fail();
+    } catch(e) {
+      done();
+    }
+  });
+  it("should be exception with multiple args", function(done) {
+    var command = new CloneCommand(api);
+    try {
+      command.run([1, 2], {});
+      assert.fail();
+    } catch(e) {
+      done();
+    }
+  });
+  it("should fail with exam option", function(done) {
+    var command = new CloneCommand(api);
+    command.run([1], {
+      user: "shunjikonishi",
+      password: "password",
+      exam: true
+    }).then(function(result) {
+      assert.equal(result.succeed, false);
+      done();
+    });
+  });
+  it("should fail with invalid password", function(done) {
+    var command = new CloneCommand(api);
+    command.run([1], {
+      user: "shunjikonishi",
+      password: "---"
+    }).then(function(result) {
+      assert.equal(result.succeed, false);
+      done();
+    });
+  });
+  it("should fail with invalid resultId", function(done) {
+    var command = new CloneCommand(api);
+    command.run([resultId + 9999], {
+      user: "shunjikonishi",
+      password: "password"
+    }).then(function(result) {
+      assert.equal(result.succeed, false);
+      done();
+    });
+  });
+  it("should succee with valid resultId", function(done) {
+    var command = new CloneCommand(api);
+    command.run([resultId], {
+      user: "shunjikonishi",
+      password: "password"
+    }).then(function(result) {
+      assert.equal(result.succeed, true);
+      var basePath = "shunjikonishi-" + resultId + "/";
+      assert.ok(fs.existsSync(basePath + "README.md"));
+      assert.ok(fs.existsSync(basePath + "test/test3.js"));
+      done();
+    });
+  });
+});
+
+function initData(done) {
+  db.deleteAll().then(function() {
+    db.create(require(app.fromSpecRoot("/fixtures/challengeViewer.json"))).then(function(data) {
+      done(data);
+    });
+  });
+}
