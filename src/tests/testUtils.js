02a0b37681ec03e8ac6028284fede62d2c8f17bf
shunjikonishi <swandouble@gmail.com>
2015-07-31 15:47:12 +0900

    Add mavenTestRunner

--- src/tests/testUtils.js
+++ src/tests/testUtils.js
@@ -3,10 +3,13 @@
 var TestRunner      = require("./testRunner");
 var MochaTestRunner = require("./mochaTestRunner");
 var SbtTestRunner   = require("./sbtTestRunner");
+var MavenTestRunner = require("./mavenTestRunner");
 
 var frameworks = [
   "mocha",
-  "sbt"
+  "sbt",
+  "mvn",
+  "maven"
 ];
 
 function availableFrameworks() {
@@ -23,6 +26,9 @@ function createTestRunner(name, args, cwd) {
       return new MochaTestRunner(args, cwd);
     case "sbt":
       return new SbtTestRunner(args, cwd);
+    case "maven":
+    case "mvn":
+      return new MavenTestRunner(args, cwd);
     default:
       var cmd = [name].concat(args).join(" ");
       return new TestRunner(cmd, cwd);
