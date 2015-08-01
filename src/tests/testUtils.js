46aa22b25c52c54b97044353ea0f1790af96a2e3
shunjikonishi <swandouble@gmail.com>
2015-08-01 09:11:40 +0900

    Add commonTestCounter

--- src/tests/testUtils.js
+++ src/tests/testUtils.js
@@ -31,8 +31,30 @@ function createTestRunner(name, args, cwd) {
       return new MavenTestRunner(args, cwd);
     default:
       var cmd = [name].concat(args).join(" ");
-      return new TestRunner(cmd, cwd);
+      var runner = new TestRunner(cmd, cwd);
+      runner.onStdout(function(data) {
+        commonTestCounter(runner, data);
+      });
+      return runner;
   }
+}
+
+function commonTestCounter(runner, data) {
+  //Ruby tes-tunit
+  //2 tests, 2 assertions, 0 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
+  function rubyTestUnit() {
+    var testsIndex = array.indexOf("tests,");
+    var failuresIndex = array.indexOf("failures,");
+    var errorsIndex = array.indexOf("errors,");
+    if (testsIndex !== -1 && failuresIndex !== -1 && errorsIndex !== -1) {
+      runner.failureCount = parseInt(array[failuresIndex - 1]) + parseInt(array[errorsIndex - 1]);
+      runner.successCount = parseInt(array[testsIndex - 1]) - runner.failureCount;
+      return true;
+    }
+    return false;
+  }
+  var array = data.split(" ");
+  if (rubyTestUnit()) { return;}
 }
 
 module.exports = {
