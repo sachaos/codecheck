4068f30ca17c7bfb25972abd7d4243a961f4ff44
shunjikonishi <swandouble@gmail.com>
2015-08-03 10:39:36 +0900

    Support specs2 output in SbtTestRunnber

--- src/tests/sbtTestRunner.js
+++ src/tests/sbtTestRunner.js
@@ -5,6 +5,8 @@ var TestRunner = require("./testRunner");
 /**
  * scalatest outputs following line at end of test
  * [info] Tests: succeeded 114, failed 0, canceled 0, ignored 0, pending 0
+ * specs2 outputs folllowing line at end of test
+ * [error] Failed: Total 4, Failed 1, Errors 0, Passed 3
  */
 function SbtTestRunner(args, cwd) {
   function hasTestArgument(args) {
@@ -14,13 +16,31 @@ function SbtTestRunner(args, cwd) {
     return args[0] === "test" || args[0] === "testOnly";
   }
   function onStdout(data) {
-    var array = data.split(" ");
-    var succeededIndex = array.indexOf("succeeded");
-    var failedIndex = array.indexOf("failed");
-    if (succeededIndex !== -1 && failedIndex !== -1 && failedIndex + 1 < array.length) {
-      self.successCount = parseInt(array[succeededIndex + 1].replace(",", ""));
-      self.failureCount = parseInt(array[failedIndex + 1].replace(",", ""));
+    function scalaTest() {
+      var succeededIndex = array.indexOf("succeeded");
+      var failedIndex = array.indexOf("failed");
+      if (succeededIndex !== -1 && failedIndex !== -1 && failedIndex + 1 < array.length) {
+        self.successCount = parseInt(array[succeededIndex + 1].replace(",", ""));
+        self.failureCount = parseInt(array[failedIndex + 1].replace(",", ""));
+        return true;
+      }
+      return false;
     }
+    function specs2() {
+      var passedIndex = array.indexOf("Passed");
+      var failedIndex = array.indexOf("Failed");
+      var errorsIndex = array.indexOf("Errors");
+      if (passedIndex !== -1 && failedIndex !== -1 && errorsIndex !== -1 && passedIndex + 1 < array.length) {
+        self.successCount = parseInt(array[passedIndex + 1].replace(",", ""));
+        self.failureCount = parseInt(array[failedIndex + 1].replace(",", "")) +
+                            parseInt(array[errorsIndex + 1].replace(",", ""));
+        return true;
+      }
+      return false;
+    }
+    var array = data.split(" ");
+    if (scalaTest()) { return;}
+    if (specs2()) { return;}
   }
 
   var self = this;
