e37d83716b0fced08b7b322a388d7b2f7e9bf916
shunjikonishi <swandouble@gmail.com>
2015-10-02 01:44:09 +0900

    Handle MONO_PATH in NUnitTestRunner

--- src/tests/nunitTestRunner.js
+++ src/tests/nunitTestRunner.js
@@ -1,5 +1,6 @@
 "use strict";
 
+var path       = require("path");
 var TestRunner = require("./testRunner");
 
 /**
@@ -7,6 +8,25 @@ var TestRunner = require("./testRunner");
  * Tests run: 22, Failures: 0, Not run: 0, Time: 0.414 seconds
  */
 function NUnitTestRunner(args, cwd) {
+  function initArgs() {
+    var ret = args || [];
+    var monoPath = process.env.MONO_PATH;
+    if (!monoPath) {
+      return ret;
+    }
+    var added = false;
+    for (var i=0; i<ret.length; i++) {
+      if (ret[i].indexOf("-lib:") === 0) {
+        ret[i] += path.sep + monoPath;
+        added = true;
+        break;
+      }
+    }
+    if (!added) {
+      ret.unshift("-lib:" + monoPath);
+    }
+    return ret;
+  }
   function onStdout(data) {
     var regex = /^Tests run: (\d+), Failures: (\d+),/;
     var match = data.match(regex);
@@ -27,7 +47,7 @@ function NUnitTestRunner(args, cwd) {
   var self = this;
   this.init();
   this.cmd = "nunit-console";
-  this.args = args;
+  this.args = initArgs();
   this.cwd = cwd;
 
   this.onStdout(onStdout);
