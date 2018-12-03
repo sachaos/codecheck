30bf27a40aedcf822da64db3d7074825e2dcfba2
shunjikonishi <swandouble@gmail.com>
2018-12-03 13:20:46 +0900

    small fix

--- test/test_runner/testRunner.test.js
+++ test/test_runner/testRunner.test.js
@@ -9,7 +9,7 @@ describe("TestRunner", function() {
         baseDirectory: "test/test_runner/menial-attack/test",
         language: "ja"
       }, require("./menial-attack/test/settings.json"));
-      const testcases = require(".//menial-attack/test/basic_testcases.json");
+      const testcases = require("./menial-attack/test/basic_testcases.json");
       const runner = codecheck.testRunner(settings, "node test/test_runner/menial-attack/solution.js");
 
       runner.runAll(testcases);
@@ -20,7 +20,7 @@ describe("TestRunner", function() {
         baseDirectory: "test/test_runner/menial-attack/test",
         language: "ja"
       }, require("./menial-attack/test/settings.json"));
-      const testcases = require(".//menial-attack/test/basic_testcases.json");
+      const testcases = require("./menial-attack/test/basic_testcases.json");
       const runner = codecheck.testRunner(settings, "node test/test_runner/menial-attack/solution.partial.js");
 
       runner.runAll(testcases);
@@ -33,7 +33,7 @@ describe("TestRunner", function() {
         baseDirectory: "test/test_runner/menial-attack/test",
         language: "ja"
       }, require("./menial-attack/test/settings.stdin.json"));
-      const testcases = require(".//menial-attack/test/basic_testcases.json");
+      const testcases = require("./menial-attack/test/basic_testcases.json");
       const runner = codecheck.testRunner(settings, "node test/test_runner/menial-attack/solution.stdin.js");
 
       runner.runAll(testcases);
@@ -41,48 +41,48 @@ describe("TestRunner", function() {
 
   });
 
-  // describe("input = file, output = file", function() {
-  //   describe("menial-attack - all pass", function() {
-  //     const settings = Object.assign({
-  //       baseDirectory: "test/test_runner/menial-attack/test",
-  //       language: "ja"
-  //     }, require("./menial-attack/test/settings.file.json"));
-  //     const testcases = require(".//menial-attack/test/basic_testcases.json");
-  //     const runner = codecheck.testRunner(settings, "node test/test_runner/menial-attack/solution.file.js");
+  describe("input = file, output = file", function() {
+    describe("menial-attack - all pass", function() {
+      const settings = Object.assign({
+        baseDirectory: "test/test_runner/menial-attack/test",
+        language: "ja"
+      }, require("./menial-attack/test/settings.file.json"));
+      const testcases = require("./menial-attack/test/basic_testcases.json");
+      const runner = codecheck.testRunner(settings, "node test/test_runner/menial-attack/solution.file.js");
 
-  //     runner.runAll(testcases);
-  //   });
+      runner.runAll(testcases);
+    });
 
-  // });
+  });
 
-  // describe("with infinite loop", function() {
-  //   describe("menial-attack - infinite loop", function() {
-  //     const settings = Object.assign({
-  //       baseDirectory: "test/test_runner/menial-attack/test",
-  //       language: "ja"
-  //     }, require("./menial-attack/test/settings.json"));
-  //     const testcases = require(".//menial-attack/test/basic_testcases.json");
-  //     const runner = codecheck.testRunner(settings, "node test/infinite/infinite.js");
+  describe("with infinite loop", function() {
+    describe("menial-attack - infinite loop", function() {
+      const settings = Object.assign({
+        baseDirectory: "test/test_runner/menial-attack/test",
+        language: "ja"
+      }, require("./menial-attack/test/settings.json"));
+      const testcases = require("./menial-attack/test/basic_testcases.json");
+      const runner = codecheck.testRunner(settings, "node test/infinite/infinite.js");
 
-  //     runner.runAll(testcases);
-  //   });
+      runner.runAll(testcases);
+    });
 
-  // });
+  });
 
-  // describe("with judge", function() {
-  //   describe("menial-attack - with judge", function() {
-  //     const settings = Object.assign({
-  //       baseDirectory: "test/test_runner/menial-attack/test",
-  //       language: "ja",
-  //       judge: {
-  //         command: "node test/test_runner/menial-attack/judge.js"
-  //       }
-  //     }, require("./menial-attack/test/settings.json"));
-  //     const testcases = require(".//menial-attack/test/basic_testcases.json");
-  //     const runner = codecheck.testRunner(settings, "node test/test_runner/menial-attack/solution.partial.js");
+  describe("with judge", function() {
+    describe("menial-attack - with judge", function() {
+      const settings = Object.assign({
+        baseDirectory: "test/test_runner/menial-attack/test",
+        language: "ja",
+        judge: {
+          command: "node test/test_runner/menial-attack/judge.js"
+        }
+      }, require("./menial-attack/test/settings.json"));
+      const testcases = require("./menial-attack/test/basic_testcases.json");
+      const runner = codecheck.testRunner(settings, "node test/test_runner/menial-attack/solution.partial.js");
 
-  //     runner.runAll(testcases);
-  //   });
+      runner.runAll(testcases);
+    });
 
-  // });
+  });
 });
